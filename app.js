99e2ada9cf0d03afaeb255cc93637ebdf76796cb
Jeevan N <jeevann@BigAdmins-MacBook-Pro.local>
2016-11-16 14:53:29 +0530

    bell notification optimisation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 var inboxUrl = 'http://172.16.66.81:6633/';
-var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'myApp.header', 'myApp.movies', 'myApp.payment', 'myApp.notify', 'myApp.profile']).config(['$routeProvider', function($routeProvider) {
+var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'myApp.header', 'myApp.movies', 'myApp.payment', 'myApp.notify', 'myApp.profile']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/home'
     });
@@ -12,18 +12,15 @@ var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'm
         baseUrl: inboxUrl,
         bookMovieApiUrl: inboxUrl + 'is/api/book-movie'
     }
-}).directive("bellNotification", ['UserNotificationService', '$filter', function(UserNotificationService, $filter) {
+}).directive("bellNotification", ['UserNotificationService', '$filter', function (UserNotificationService, $filter) {
     return {
         template: `<span id='notification_count' ng-if='notViewedCount>0'>{{notViewedCount}}</span>`,
-        link: function(scope, element, attrs) {
-
-            var notifications = UserNotificationService.getAllNotifications();
-
-            notifications.then(function(notification) {
-                scope.notifications = notification;
-                scope.notViewedCount = UserNotificationService.updateNotViewedCount(scope.notifications);
+        scope: true,
+        link: function (scope, element, attrs) {
+            UserNotificationService.getAllNotifications().then(function (notifications) {
+                scope.notViewedCount = UserNotificationService.updateNotViewedCount(notifications);
+                scope.notifications = notifications;
             });
-            
         }
     };
 }]);
