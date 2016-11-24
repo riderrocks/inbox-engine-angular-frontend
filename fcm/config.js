9badcb16383460a66f3e0465381053ed50321cc0
Jeevan N <jeevann@BigAdmins-MacBook-Pro.local>
2016-11-24 14:43:20 +0530

    fcm library

---
+++ fcm/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAV18UcMQ:APA91bE_UbJyIm2rbNhkMbfBtoFcUYK6ozbvE4kDaQpryFxUbkZKR38Y-RAm4K3uMM4cVJGv4nYPTn0C6yg-xmdnTauBmY4_lFPMKbIG2O6zwa5pT9JszVF6hsU2684IgnK2bQHWpxFpy2rJCHhuzeSS9DjR38lYuQ'
+};
