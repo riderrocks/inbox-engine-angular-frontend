e4b498ceaef20db12a738f6690cc6f9ecb4e7798
Jeevan N <jeevan.n@bookmyshow.com>
2016-12-16 12:06:39 +0000

    live message push for edit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var inboxUrl = 'http://172.16.65.7:6633/';
 var socketUrl = 'http://172.16.65.7:4001';
 
-var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'myApp.header', 'myApp.movies', 'myApp.payment', 'myApp.notify', 'myApp.profile']).config(['$routeProvider', function($routeProvider) {
+var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'myApp.header', 'myApp.movies', 'myApp.payment', 'myApp.notify', 'myApp.profile']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/home'
     });
@@ -17,27 +17,34 @@ var app = angular.module('myApp', ['ngRoute', 'myApp.register', 'myApp.home', 'm
         bookMovieApiUrl: inboxUrl + 'is/api/book-movie'
     }
 }).directive("bellNotification", ['UserNotificationService', '$filter', 'SocketIoService', function (UserNotificationService, $filter, SocketIoService) {
-
+    var pushMessage = function (message, scope) {
+        var notify = UserNotificationService.prepareData([message.notify]);
+        if (message.notify.isEdited) {
+            var index;
+            index = scope.notifications.id.indexOf(message.notify._id);
+            if (index >= 0) {
+                if (scope.notifications.data[index].viewed === true) {
+                    scope.notViewedCount += 1;
+                }
+                scope.notifications.data[index] = notify.data[0];
+                scope.notifications.id[index] = notify.id[0];
+            }
+        } else {
+            scope.notifications.data.unshift(notify.data[0]);
+            scope.notifications.id.unshift(notify.id[0]);
+            scope.notViewedCount += 1;
+        }
+    }
     return {
         template: `<span id='notification_count' ng-if='notViewedCount>0'>{{notViewedCount}}</span>`,
         scope: true,
         link: function (scope, element, attrs) {
             SocketIoService.on('notification', function (message) {
-                scope.notViewedCount += 1;
-                console.log(message.notify);
-                var notify = UserNotificationService.prepareData([message.notify]);
-                scope.notifications.data.unshift(notify.data[0]);
-                scope.notifications.id.unshift(notify.id[0]);
-
-
+                pushMessage(message, scope);
             });
             var memberId = localStorage.userId;
             SocketIoService.on('notification_' + memberId, function (message) {
-                scope.notViewedCount += 1;
-                console.log(message.notify);
-                var notify = UserNotificationService.prepareData([message.notify]);
-                scope.notifications.data.unshift(notify.data[0]);
-                scope.notifications.id.unshift(notify.id[0]);
+                pushMessage(message, scope);
             });
         }
     };
